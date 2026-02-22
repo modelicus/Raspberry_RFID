b130efd3ce03deb33e667faa4bdde4223dcc156e
modelicus <206447528+modelicus@users.noreply.github.com>
2026-02-22 18:58:42 +0100

    Trying to read entire UID

--- rfid_reader.py
+++ rfid_reader.py
@@ -29,21 +29,51 @@ class RFIDReader:
             return None
 
 
-        # Full anticollision + select
-        # This returns entire UID
+        # Anti-collision
 
-        (status, uid) = self.reader.MFRC522_SelectTagSN()
+        (status, uid) = self.reader.MFRC522_Anticoll()
 
         if status != self.reader.MI_OK:
             return None
 
 
+        # uid example:
+        # [0x88, 0x04, 0x13, 0x25, 0xBA]
+        #
+        # 0x88 = Cascade Tag
+        # Means UID continues
+
+
+        uid_bytes = uid[:4]
+
+
+        # Remove cascade byte if present
+
+        if uid_bytes[0] == 0x88:
+
+            uid_bytes = uid_bytes[1:]
+
+
+            # Select first cascade level
+
+            self.reader.MFRC522_SelectTag(uid)
+
+
+            # Read second cascade level
+
+            (status2, uid2) = self.reader.MFRC522_Anticoll()
+
+            if status2 != self.reader.MI_OK:
+                return None
+
+
+            uid_bytes += uid2[:4]
+
+
         # Convert to HEX string
-        # Example:
-        # 041325BABD370289
 
         uid_str = "".join(
-            f"{byte:02X}" for byte in uid
+            f"{byte:02X}" for byte in uid_bytes
         )
 
 
@@ -54,7 +84,6 @@ class RFIDReader:
         if uid_str in self.last_seen:
 
             if now - self.last_seen[uid_str] < self.debounce_seconds:
-
                 return None
 
 
